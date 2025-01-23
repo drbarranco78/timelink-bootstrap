95f10347270acc23bbf68feb12152c709d462da5
Daniel Rodriguez Barranco <drbarranco78@gmail.com>
2025-01-23 17:36:46 +0000

    middleware

--- routes/web.php
+++ routes/web.php
@@ -20,66 +20,10 @@ Route::get('/login', function (){
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/private', function () {
-        return view('private'); // O contenido según sea necesario
+        return view('private'); 
     });
     Route::get('/admin', function () {
-        return view('admin'); // O contenido según sea necesario
+        return view('admin'); 
     });
 });
 
-// Route::get('login', function (){
-//     return view('index');
-// });
-
-// Route::middleware(['web'])->group(function () {
-//     Route::post('/login', [UserController::class, 'login']);
-// });
-
-// Route::get('/login', function () {
-//     if (Auth::user()){
-//         return view('private');
-//     }else{
-//         echo 'Valor de check '.Auth::check();
-        
-//     }    
-// })->name('login');
-
-// Route::view('/private', 'private')->name('private')->middleware('auth');
-
-// Route::middleware(['auth'])->group(function () {
-//     Route::get('/private', function () {
-//         return view('private'); // O contenido según sea necesario
-//     });
-// });
-// Route::middleware([RedirectIfNotAuthenticated::class])->group(function () {
-
-//     Route::get('/private', function () {        
-//         return view('private');     
-//     });
-
-//     Route::get('/admin', function () {
-//         return 'Panel de administración';
-//     });
-
-// });
-
-
-// Route::middleware('auth')->get('/private', function () {
-//     return view('private'); 
-// });
-
-
-
-// Route::get('/login', function (){
-//     return 'Login page';
-// })->name('login');
-
-// Route::post('/login', [AuthController::class, 'login'])->name('login');
-
-// Route::get('/private', function () {
-//     return view('private');
-// })->middleware(RedirectIfNotAuthenticated::class);
-
-// Route::group(['middleware' => ['web']], function () {
-//     Route::post('/login', [UserController::class, 'login']);
-// });
